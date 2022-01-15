693e0a280c23b16431cfca68b8f2c40f0993cb0f
tim901231 <b09901020@ntu.edu.tw>
2022-01-15 23:45:24 +0800

    try

--- src/server.js
+++ src/server.js
@@ -11,7 +11,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: "http://localhost:3000",
+    origin: "https://epedemic.herokuapp.com/",
     methods: ["GET", "POST"],
   },
 });
@@ -95,7 +95,7 @@ db.once("open", () => {
       console.log("a user disconnected");
     });
   });
-  const PORT = process.env.port || 5000;
+  const PORT = process.env.PORT || 5000;
   server.listen(PORT, () => {
     console.log(`Server is up on port ${PORT}.`);
   });
